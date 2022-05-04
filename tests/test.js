db2a9231d905fcff1dbd1cca8ddcd98ba16eb3c3
Ranjan Nagaraju <rannagar@publicisgroupe.net>
2022-05-04 12:12:12 +0530

    Created hello world node js app for ci/cd demo

---
+++ tests/test.js
@@ -0,0 +1,10 @@
+const request = require("supertest")
+
+const app = require('../index')
+
+describe('GET/', () => {
+    it('respond with hello world', (done) => {
+        request(app).get('/').expect("Hello world", done);
+    })
+})
+
